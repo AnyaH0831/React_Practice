d82dd4c6355b3a2bf9fdb5fa01bf56dcf57eb7bd
AnyaH0831 <anyahuang0831@gmail.com>
2026-01-03 05:24:39 -0500

    navbar and styling

---
+++ blog/src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Th Anya Blog</h1>
+            <div className="links">
+                <a href="/">Homes</a>
+                <a href="/create">New Blog</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
