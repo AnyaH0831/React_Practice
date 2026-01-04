1294693d63115a78ad47fffa6cd1b7ebac7ed9e8
AnyaH0831 <anyahuang0831@gmail.com>
2026-01-04 06:55:54 -0500

    more react practice

--- blog/src/Home.js
+++ blog/src/Home.js
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        {title: 'My new website', body: 'lorem ipsum...', author: 'mario', id:1},
+        {title: 'My new website 2', body: 'lorem ipsum...', author: 'mario', id:2}
+    ]);
+
     return (  
         <div className="home">
-            <h2>Home Page</h2>
+       
+           {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written By {blog.author}</p>
+                </div>
+           ))}
         </div>
     );
 }
